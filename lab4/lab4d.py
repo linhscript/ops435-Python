27fa41840719570098b0ed3424f048eacc9e1ea6
Linh Van Ha <vlha@myseneca.ca>
2019-01-24 18:37:34 -0500

    Done lab4

--- lab4/lab4d.py
+++ lab4/lab4d.py
@@ -7,18 +7,20 @@ str2 = 'Seneca College'
 num1 = 1500
 num2 = 1.50
 
-def first_five():
+def first_five(self):
     # Place code here - refer to function specifics in section below
-
-def last_seven():
+    return self[0:5]
+def last_seven(self):
     # Place code here - refer to function specifics in section below
-
-def middle_number():
+    return self[-7:]
+def middle_number(self):
     # Place code here - refer to function specifics in section below
-
-def first_three_last_three():
+    #return str(self)[(len(str(self))//2)-1:(len(str(self))//2)+1]
+    return str(self)[1:3]
+    
+def first_three_last_three(self1,self2):
     # Place code here - refer to function specifics in section below
-
+    return str(self1[0:3]+self2[-3:])
 
 if __name__ == '__main__':
     print(first_five(str1))
