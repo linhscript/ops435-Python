465c74d35a384e4d27612561e8eac246836e7610
Linh Ha Van <havanlinh304@gmail.com>
2019-02-03 17:00:33 -0500

    123

--- Assignment/Assignment1.py
+++ Assignment/Assignment1.py
@@ -7,48 +7,48 @@ def dbda(var1,var2):
   return result
 
 def validate(var1):
-	if len(var1) != 8:   # Check date format
+	if (len(var1) != 8):   # Check date format
 		print("Wrong date format")
 	else:
-	    year = int(var1[0:4])
-	    month = int(var1[4:6])
-	    day = int(var1[6:])
+		year = int(var1[0:4])
+		month = int(var1[4:6])
+		day = int(var1[6:])
 	    
-	    if (year % 4 == 0):
-	      if (year % 100 == 0):
-	        if (year % 400 ==0):
-	          feb = 29
-	        else:
-	          feb = 28
-	      else:
-	        feb = 29
-	    else:
-	      feb = 28
+		if (year % 4 == 0):
+			if (year % 100 == 0):
+				if (year % 400 ==0):
+					feb = 29
+				else:
+					feb = 28
+			else:
+				feb = 29
+		else:
+			feb = 28
 	    ## Done Check Leaf year
-	    
 
-	    mon_max = { 1:31, 2:feb, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+		mon_max = { 1:31, 2:feb, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
 
 
 	    # Check month and day are valid or not
-	    if month not in mon_max.keys():
-	    	print("Wrong month input")
-	    else:
-	    	day_max = mon_max[month]
-	    	if day not in range (1,day_max+1):    ### Plus 1 to take the last value
-	    		print("Wrong day input")
+		if month not in mon_max.keys():
+			print("Wrong month input")
+		else:
+			day_max = mon_max[month]
+			if day not in range (1,day_max+1):    ### Plus 1 to take the last value
+				print("Wrong day input")
 
-	return mon_max
+		return mon_max
 
 def tomorow(var1):
 	validate(var1)
+
 	
 
 #def yesterday(var1, num)
 
 
 if __name__ == "__main__":
-  var1 = '20190331'
+  var1 = '2019033'
   var2 = +3
   var3 = -3
   #dbda(var1,var2)
--- test.py
+++ test.py
@@ -3,21 +3,19 @@
 #print(sys.version)
 
 def validate(var1):
-	mon_max = { 1:31, 2:30, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+	if (len(var1) != 8):   # Check date format
+		print("Wrong date format")
+	else:
+		mon_max = { 1:31, 2:31, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+		print("Wrong day input")
 	return mon_max
-
-def tomorow(var1, num):
-	test= validate(var1)
-	print(test)
-
-#def yesterday(var1, num)
 
 
 if __name__ == "__main__":
-  var1 = '20190331'
+  var1 = '201903311'
   var2 = +3
   var3 = -3
   #dbda(var1,var2)
   #dbda(var1,var3)
   validate(var1)
-  tomorow(var1,4)
+
