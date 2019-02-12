f49484af30a275513715dafe182837bfeed37666
Linh Ha Van <havanlinh304@gmail.com>
2019-02-11 23:42:28 -0500

    12

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -5,7 +5,7 @@ class Student:
     # Define the name and number when a student object is created, ex. student1 = Student('john', 025969102)
     def __init__(self, name, number):
         self.name = name
-        self.number = number
+        self.number = int(number)
         self.courses = {}
 
     # Return student name and number
