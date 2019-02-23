029558261f62492464efe27f4e3fe6bd7e78b074
Linh Ha Van <havanlinh304@gmail.com>
2019-02-22 23:44:56 -0500

    123

--- Assignment/test.py
+++ Assignment/test.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 
 #file = usage_data_file
-f = open('usage_data_file','r')
-a = f.readlines()
-f.close()
-#all_name = {}
-for name in a:
-	all_name.append(name.split()[0])
-for item in all_name:
+def read_login_rec(filelist):
 
+	f = open(filelist,'r')
+	login_rec = f.readlines()
+	f.close()
+	return login_rec
+
+def get_list (list_record,position):
+	res_list = set()
+	for item in list_record:
+		res_list.add(item.split()[position])
+
+	for i in res_list:
+		print (i)
+
+if __name__ == '__main__':
+	import argparse
+	filename = 'usage_data_file'
+	parser = argparse.ArgumentParser(description = "Usage Report based on the last command",
+	 epilog = "Copyright 2018 - Linh Van Ha")
+
+	parser.add_argument('-l {user,host}', '--list', choices = ['user','host']
+		,help='generate user name or remote host IP from the given files')
+	args = parser.parse_args()
+
+	login_rec = read_login_rec (filename)
+	if args.list == 'user':
+		number = 0
+	else:
+		number = 2
+	get_list (login_rec,number)
