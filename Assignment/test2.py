b2ce71f5e839e532e6f0ec3abde4240e767a052e
Linh Van Ha <vlha@myseneca.ca>
2019-02-24 11:58:01 -0500

    In progress

--- Assignment/test2.py
+++ Assignment/test2.py
@@ -1,10 +1,18 @@
-import time
-#t = time.asctime()
-#print(t)
+def read_login_rec(filelist):
 
-name = "Daily Report for "
-host = "10.40.105.130"
-print(name,host)
-sign = len(name+host)
-print(sign*'=')
-print("Date"+" "*10+"Usage in Seconds")
+	f = open(filelist,'r')
+	login_rec = f.readlines()
+	f.close()
+	return login_rec
+
+if __name__ == '__main__':
+	import time
+
+	filelist = 'usage_data_file'
+	test = read_login_rec(filelist)
+	result = test[0].split()
+	#print(result)
+
+	t = (2009, 2, 17, 17, 3, 38, 8, 48, 0)
+	t = time.strftime('%b %d %Y %H:%M:%S',t)
+	print(t)
