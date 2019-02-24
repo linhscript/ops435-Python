6e3da6293a6834ccf8d35bedfcd437497cf9673c
Linh Ha Van <havanlinh304@gmail.com>
2019-02-24 16:34:51 -0500

    123

--- Assignment/test.py
+++ Assignment/test.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-
-#file = usage_data_file
 def read_login_rec(filelist):
 
 	f = open(filelist,'r')
@@ -8,65 +5,22 @@ def read_login_rec(filelist):
 	f.close()
 	return login_rec
 
-def get_list (list_record,position):
-	res_list = set()
-	for item in list_record:
-		res_list.add(item.split()[position])
-
-	#for i in sorted(res_list):
-	#	print(i)
-	print(*sorted(res_list),sep = "\n")
-
-def cal_daily_usage(subject,login_recs):
-	''' docstring for this function
-	generate daily usage report for the given 
-	subject (user or remote host)'''
-	text = "Daily Usage Report for "
-	print(text+str(subject))
-	print(len(text+str(subject))*'=')
-	print("Date"+" "*10+"Usage in Seconds")
-	
-	return daily_usage
-
 if __name__ == '__main__':
-	import argparse
-	filename = 'usage_data_file'
-	parser = argparse.ArgumentParser(description = "Usage Report based on the last command",
-	 epilog = "Copyright 2018 - Linh Van Ha")
-
-	parser.add_argument('filename',metavar = 'F', nargs='*',help='list of files to be processed')
-	parser.add_argument('-l', '--list', choices = ['user','host']
-		,help='generate user name or remote host IP from the given files')
-	parser.add_argument('-r', '--rhost', help='usage report for the given remote host IP')
-	parser.add_argument('-t', '--type', choices = ['daily','weekly','monthly']
-		,help='type of report: daily, weekly, and monthly')
-	parser.add_argument('-u', '--user' ,help='usage report for the given user name')
-	parser.add_argument('-v', '--verbose',metavar='',help='tune on output verbosity')
-	args = parser.parse_args()
-
-###################################################################
-	login_rec = []
-	for file in args.filename:
-		login_rec.extend(read_login_rec(file))
-
-	if args.list:
-		if args.list == 'user':
-			number = 0
-		else:
-			number = 2
-		get_list(login_rec,number)
+	import time
 	
+	filelist = 'test_data'
+	test = read_login_rec(filelist)
+	total = 0
+	for item in test:
+		time1 = time.strptime(' '.join(item.split()[3:8]),"%a %b %d %H:%M:%S %Y")
+		time2 = time.strptime(' '.join(item.split()[9:14]),"%a %b %d %H:%M:%S %Y")
+		doy1 = time.strftime("%j",time1) # return day of year
+		doy2 = time.strftime("%j",time2)
+ 
+		if doy1 == doy2: # if same day
+			total += abs(time.mktime(time1) - time.mktime(time2))
+		else:
+			#while doy1 != doy2:
+			print(True)
 
-###################################################################
-
-	if args.rhost:
-		subject = args.rhost
-	elif args.user:
-		subject = args.user
-
-	if args.type == 'daily':
-		cal_daily_usage(subject,)
-	elif args.type == 'weekly':
-		cal_weekly_usage(subject,)
-	else:
-		cal_monthly_usage(subject,)
+	print(total)
--- Assignment/test2.py
+++ Assignment/test2.py
@@ -11,26 +11,7 @@ if __name__ == '__main__':
 	filelist = 'usage_data_file'
 	test = read_login_rec(filelist)
 	result = test[0].split()
-	#print(result)
-	#Tue Feb 13 16:22:00 2018 - Tue Feb 13 16:45:00 2018
-	a = time.strptime("Tue Feb 13 16:53:42 2018","%a %b %d %H:%M:%S %Y")
-	b = time.strptime("Tue Feb 13 16:57:02 2018","%a %b %d %H:%M:%S %Y")
-	c = time.strptime("Tue Feb 13 16:22:00 2018","%a %b %d %H:%M:%S %Y")
-	d = time.strptime("Tue Feb 13 16:45:00 2018","%a %b %d %H:%M:%S %Y")
-
-	#print(a)
-	#print(b)
-	s = time.strftime("%H:%M:%S",a)
-	j = time.strftime("%H:%M:%S",b)
-
-	#print(s)
-	#print(j)
-	res1 = time.mktime(a)
-	res2 = time.mktime(b)
-	res3 = time.mktime(c)
-	res4 = time.mktime(d)	
-	total1 = abs(res1-res2)
-	total2 = abs(res3-res4)
-	total = total1+total2
-	print(total)
-	#print(time.mktime(s))
+	time1 = time.strptime(' '.join(result[3:8]),"%a %b %d %H:%M:%S %Y")
+	
+	print(time1)
+	
