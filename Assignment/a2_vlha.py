468f1cbbc91e0590e6d3d2a615754f3997d48501
Linh Ha Van <havanlinh304@gmail.com>
2019-02-24 14:44:45 -0500

    12

--- Assignment/a2_vlha.py
+++ Assignment/a2_vlha.py
@@ -22,19 +22,30 @@ def get_login_rec():
     return login_recs
  
 def read_login_rec(filelist):
-    ''' docstring for this function
-    get records from given filelist
-    open and read each file from the filelist
-    filter out the unwanted records
-    add filtered record to list (login_recs)''' 
-    [ put your python code for this function here ]
+
+    f = open(filelist,'r')
+    login_rec = f.readlines()
+    f.close()
     return login_rec
+
+def get_list (list_record,position):
+    res_list = set()
+    for item in list_record:
+        res_list.add(item.split()[position])
+
+    #for i in sorted(res_list):
+    #   print(i)
+    print(*sorted(res_list),sep = "\n")
 
 def cal_daily_usage(subject,login_recs):
     ''' docstring for this function
     generate daily usage report for the given 
     subject (user or remote host)'''
-    [ put your python code for this function here ]
+    text = "Daily Usage Report for "
+    print(text+str(subject))
+    print(len(text+str(subject))*'=')
+    print("Date"+" "*10+"Usage in Seconds")
+    
     return daily_usage
 
 def cal_weekly_usage(subject,login_recs):
@@ -53,9 +64,43 @@ def cal_monthly_usage(subject,login_recs):
      
 if __name__ == '__main__':
     import argparse
-    ...
-    [ code to retrieve command line argument using the argparse module [
-    ...
-    [ based on the command line option, generate and print
-      the requested usage report ]
-    ...
+    filename = 'usage_data_file'
+    parser = argparse.ArgumentParser(description = "Usage Report based on the last command",
+     epilog = "Copyright 2018 - Linh Van Ha")
+
+    parser.add_argument('filename',metavar = 'F', nargs='*',help='list of files to be processed')
+    parser.add_argument('-l', '--list', choices = ['user','host']
+        ,help='generate user name or remote host IP from the given files')
+    parser.add_argument('-r', '--rhost', help='usage report for the given remote host IP')
+    parser.add_argument('-t', '--type', choices = ['daily','weekly','monthly']
+        ,help='type of report: daily, weekly, and monthly')
+    parser.add_argument('-u', '--user' ,help='usage report for the given user name')
+    parser.add_argument('-v', '--verbose',metavar='',help='tune on output verbosity')
+    args = parser.parse_args()
+
+###################################################################
+    login_rec = []
+    for file in args.filename:
+        login_rec.extend(read_login_rec(file))
+
+    if args.list:
+        if args.list == 'user':
+            number = 0
+        else:
+            number = 2
+        get_list(login_rec,number)
+    
+
+###################################################################
+
+    if args.rhost:
+        subject = args.rhost
+    elif args.user:
+        subject = args.user
+
+    if args.type == 'daily':
+        cal_daily_usage(subject,)
+    elif args.type == 'weekly':
+        cal_weekly_usage(subject,)
+    else:
+        cal_monthly_usage(subject,)
--- Assignment/test2.py
+++ Assignment/test2.py
@@ -7,12 +7,30 @@ def read_login_rec(filelist):
 
 if __name__ == '__main__':
 	import time
-
+	
 	filelist = 'usage_data_file'
 	test = read_login_rec(filelist)
 	result = test[0].split()
 	#print(result)
+	#Tue Feb 13 16:22:00 2018 - Tue Feb 13 16:45:00 2018
+	a = time.strptime("Tue Feb 13 16:53:42 2018","%a %b %d %H:%M:%S %Y")
+	b = time.strptime("Tue Feb 13 16:57:02 2018","%a %b %d %H:%M:%S %Y")
+	c = time.strptime("Tue Feb 13 16:22:00 2018","%a %b %d %H:%M:%S %Y")
+	d = time.strptime("Tue Feb 13 16:45:00 2018","%a %b %d %H:%M:%S %Y")
 
-	t = (2009, 2, 17, 17, 3, 38, 8, 48, 0)
-	t = time.strftime('%b %d %Y %H:%M:%S',t)
-	print(t)
+	#print(a)
+	#print(b)
+	s = time.strftime("%H:%M:%S",a)
+	j = time.strftime("%H:%M:%S",b)
+
+	#print(s)
+	#print(j)
+	res1 = time.mktime(a)
+	res2 = time.mktime(b)
+	res3 = time.mktime(c)
+	res4 = time.mktime(d)	
+	total1 = abs(res1-res2)
+	total2 = abs(res3-res4)
+	total = total1+total2
+	print(total)
+	#print(time.mktime(s))
