30b5cf2df0b5b0ac6adf68cc2f62e74573db704f
Linh Ha Van <havanlinh304@gmail.com>
2019-02-24 23:07:21 -0500

    12

--- Assignment/test.py
+++ Assignment/test.py
@@ -4,23 +4,25 @@ def read_login_rec(filelist):
 	login_rec = f.readlines()
 	f.close()
 	return login_rec
-
 if __name__ == '__main__':
 	import time
 	
 	filelist = 'test_data'
 	test = read_login_rec(filelist)
 	total = 0
+	record_list = []
 	for item in test:
 		time1 = time.strptime(' '.join(item.split()[3:8]),"%a %b %d %H:%M:%S %Y")
 		time2 = time.strptime(' '.join(item.split()[9:14]),"%a %b %d %H:%M:%S %Y")
+		# time1 and time2 is struct_time
 		doy1 = time.strftime("%j",time1) # return day of year
 		doy2 = time.strftime("%j",time2)
  
 		if doy1 == doy2: # if same day
+			print(time.mktime(time1),time.mktime(time2))
 			total += abs(time.mktime(time1) - time.mktime(time2))
 		else:
 			#while doy1 != doy2:
-			print(True)
+			print('87,780 seconds in total and 60,300 seconds in 14 and (27,479+200=27679) in 13')
 
 	print(total)
--- Assignment/test2.py
+++ Assignment/test2.py
@@ -8,10 +8,28 @@ def read_login_rec(filelist):
 if __name__ == '__main__':
 	import time
 	
-	filelist = 'usage_data_file'
+	filelist = 'test_data'
 	test = read_login_rec(filelist)
-	result = test[0].split()
-	time1 = time.strptime(' '.join(result[3:8]),"%a %b %d %H:%M:%S %Y")
+	result = test[1].split()
 	
+	time1 = time.strptime(' '.join(result[3:8]),"%a %b %d %H:%M:%S %Y")
+	time2 = time.strptime(' '.join(result[9:14]),"%a %b %d %H:%M:%S %Y")
+	day1 = time.strftime('%j',time1)
+	day2 = time.strftime('%j',time2)
+	time3 = time.strptime(' '.join(result[3:8]),"%a %b %d %H:%M:%S %Y")
+	t = time.ctime(time.mktime(time1)).split()
+	t[3] = '23:59:59'
+	print(result[3:8],result[9:14])
+	print(t)
+	print(time.ctime(time.mktime(time1)))
+	print(day1,day2)
 	print(time1)
+	print(time2)
+
+	#Tue Feb 13 16:53:42 2018
+	next_day = time.mktime(time1) # float number
+	while day1 != day2:
+		next_day = next_day + 86400
+		day1 = time.strftime('%j',time.localtime(next_day))
+	
 	
