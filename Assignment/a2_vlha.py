981dca5a01eb189afd08a3fdf56551b1c3ccf8fc
Linh Van Ha <vlha@myseneca.ca>
2019-02-25 20:49:08 -0500

    Done

--- Assignment/a2_vlha.py
+++ Assignment/a2_vlha.py
@@ -12,6 +12,7 @@
 import os 
 import sys
 import time
+import argparse
 
 def get_login_rec():
     ''' docstring for this fucntion
@@ -43,6 +44,51 @@ def get_list (list_record,position):
     #for i in sorted(res_list):
     #   print(i)
     print(*sorted(res_list),sep = "\n")
+
+def format_record(unformat_record):
+
+    record_list = []
+    for item in unformat_record:
+        time1 = time.strptime(' '.join(item.split()[3:8]),"%a %b %d %H:%M:%S %Y")
+        time2 = time.strptime(' '.join(item.split()[9:14]),"%a %b %d %H:%M:%S %Y")
+        # time1 and time2 is struct_time
+        doy1 = time.strftime("%j",time1) # return day of year
+        doy2 = time.strftime("%j",time2)
+ 
+        if doy1 == doy2: # if same day
+            record_list.append(item.split()) # save the record to list
+        else: # this works even jump to many days
+            next_day = time.mktime(time1) # float number
+            
+            eod_time = time.ctime(next_day).split() #convert to readable time
+            old_day = item.split()
+
+            old_day[9] = eod_time[0]
+            old_day[10] = eod_time[1]
+            old_day[11] = eod_time[2]
+            old_day[12] = '23:59:59'
+            old_day[13] = eod_time[4]                           
+            record_list.append(old_day)
+            while doy1 != doy2:
+                                
+                next_day = next_day + 86400 #floating number
+                new_day = item.split()
+                new_time = time.ctime(next_day).split() # covert to readable time
+                new_day[3] = new_time[0]
+                new_day[4] = new_time[1]
+                new_day[5] = new_time[2]
+                new_day[6] = '00:00:00'
+                new_day[7] = new_time[4]
+                doy1 = time.strftime('%j',time.localtime(next_day))
+
+                if doy1 != doy2: # check if next day is on the same day with end day or not
+                    new_day[9] = new_time[0]
+                    new_day[10] = new_time[1]
+                    new_day[11] = new_time[2]
+                    new_day[12] = '23:59:59'
+                    new_day[13] = new_time[4]
+                record_list.append(new_day)
+    return record_list
 
 def cal_daily_usage(subject,login_recs):
     ''' docstring for this function
@@ -120,8 +166,6 @@ def cal_monthly_usage(subject,login_recs):
     print("Total" +" "*(l//2),total)
      
 if __name__ == '__main__':
-    import argparse
-    filename = 'usage_data_file'
     parser = argparse.ArgumentParser(description = "Usage Report based on the last command",
      epilog = "Copyright 2018 - Linh Van Ha")
 
@@ -156,48 +200,7 @@ if __name__ == '__main__':
         subject = args.user
 
     if args.type:
-        record_list = []
-        for item in login_rec:
-            time1 = time.strptime(' '.join(item.split()[3:8]),"%a %b %d %H:%M:%S %Y")
-            time2 = time.strptime(' '.join(item.split()[9:14]),"%a %b %d %H:%M:%S %Y")
-            # time1 and time2 is struct_time
-            doy1 = time.strftime("%j",time1) # return day of year
-            doy2 = time.strftime("%j",time2)
-     
-            if doy1 == doy2: # if same day
-                record_list.append(item.split()) # save the record to list
-            else: # this works even jump to many days
-                next_day = time.mktime(time1) # float number
-                
-                eod_time = time.ctime(next_day).split() #convert to readable time
-                old_day = item.split()
-
-                old_day[9] = eod_time[0]
-                old_day[10] = eod_time[1]
-                old_day[11] = eod_time[2]
-                old_day[12] = '23:59:59'
-                old_day[13] = eod_time[4]                           
-                record_list.append(old_day)
-                while doy1 != doy2:
-                                    
-                    next_day = next_day + 86400 #floating number
-                    new_day = item.split()
-                    new_time = time.ctime(next_day).split() # covert to readable time
-                    new_day[3] = new_time[0]
-                    new_day[4] = new_time[1]
-                    new_day[5] = new_time[2]
-                    new_day[6] = '00:00:00'
-                    new_day[7] = new_time[4]
-                    doy1 = time.strftime('%j',time.localtime(next_day))
-
-                    if doy1 != doy2: # check if next day is on the same day with end day or not
-                        new_day[9] = new_time[0]
-                        new_day[10] = new_time[1]
-                        new_day[11] = new_time[2]
-                        new_day[12] = '23:59:59'
-                        new_day[13] = new_time[4]
-                    record_list.append(new_day)
-
+        record_list = format_record(login_rec)
         if args.type == 'daily':
             cal_daily_usage(subject,record_list)
         elif args.type == 'weekly':
