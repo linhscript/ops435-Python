93be0017b3af0f9425740d64167e4b3b9abeb2b1
Linh Van Ha <vlha@myseneca.ca>
2019-03-07 20:44:24 -0500

    Done

--- Assignment/test3.py
+++ Assignment/test3.py
@@ -15,9 +15,21 @@ reading login/logout record files ['a2_test_data_2']
 Generating list for host
 
 '''
-def gen_text():
-	print("Files to be processed: ",args.filename)
-	print("line1\nType of args for files <class 'list'>")
+def gen_text(option):
+	if option == True:
+		print("Files to be processed: ",args.filename)
+		print("Type of args for files ",type(args.filename))
+
+		if args.list:
+			print("usage report for user: ",subject)
+			print("usage report type: ",args.type)
+			print("processing usage report for the following:")
+			print("reading login/logout record files ",args.filename)
+		else:
+			print("processing usage report for the following:")
+			print("reading login/logout record files ",args.filename)
+			print("Generating list for ",args.list)
+
 
 if __name__ == '__main__':
 	import time
--- Assignment/test_main.py
+++ Assignment/test_main.py
@@ -39,7 +39,8 @@ def read_login_rec(filelist):
     f.close()
     return login_recs
 
-def get_list (list_record,position):
+def get_list (list_record,position,option):
+    gen_text(option)
     res_list = set()
     for item in list_record:
         res_list.add(item.split()[position])
@@ -93,10 +94,11 @@ def format_record(unformat_record):
                 record_list.append(new_day)
     return record_list
 
-def cal_daily_usage(subject,login_recs):
+def cal_daily_usage(subject,login_recs,option):
     ''' docstring for this function
     generate daily usage report for the given 
     subject (user or remote host)'''
+    gen_text(option)
     text = "Daily Usage Report for "
     print(text+str(subject))
     print(len(text+str(subject))*'=')
@@ -117,10 +119,11 @@ def cal_daily_usage(subject,login_recs):
         print ("{:<11s}{:>11d}".format(str(key),daily_usage[key]))
     print("{:<11s}{:>11d}".format("Total",total))
     
-def cal_weekly_usage(subject,login_recs):
+def cal_weekly_usage(subject,login_recs,option):
     ''' docstring for this function
     generate weekly usage report for the given 
     subject (user or remote host)'''
+    gen_text(option)
     text = "Weekly Usage Report for "
     print(text+str(subject))
     print(len(text+str(subject))*'=')
@@ -141,10 +144,11 @@ def cal_weekly_usage(subject,login_recs):
         print ("{:<11s}{:>11d}".format(str(key),weekly_usage[key]))
     print("{:<11s}{:>11d}".format("Total",total))
 
-def cal_monthly_usage(subject,login_recs):
+def cal_monthly_usage(subject,login_recs,option):
     ''' docstring for this function
     generate monthly usage report fro the given
     subject (user or remote host)'''
+    gen_text(option)
     text = "Monthly Usage Report for "
     print(text+str(subject))
     print(len(text+str(subject))*'=')
@@ -164,6 +168,21 @@ def cal_monthly_usage(subject,login_recs):
     for key in sorted(monthly_usage.keys(),reverse=True):
         print ("{:<11s}{:>11d}".format(str(key),monthly_usage[key]))
     print("{:<11s}{:>11d}".format("Total",total))
+
+def gen_text(option):
+    if option == True:
+        print("Files to be processed:",args.filename)
+        print("Type of args for files",type(args.filename))
+
+        if args.list:
+            print("processing usage report for the following:")
+            print("reading login/logout record files",args.filename)
+            print("Generating list for",args.list)
+        else:
+            print("usage report for user:",subject)
+            print("usage report type:",args.type)
+            print("processing usage report for the following:")
+            print("reading login/logout record files",args.filename)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "Usage Report based on the last command",
@@ -187,26 +206,29 @@ if __name__ == '__main__':
         for file in args.filename:
             unformatted_login_rec.extend(read_login_rec(file))
 
+    if args.verbose:
+        option = True
+    else:
+        option = False
+
     if args.list:
         if args.list == 'user':
-            number = 0
+            position = 0
         else:
-            number = 2
-        get_list(unformatted_login_rec,number)
+            position = 2
+        get_list(unformatted_login_rec,position,option)
     
+    else:
+        if args.rhost:
+            subject = args.rhost
+        elif args.user:
+            subject = args.user
 
-###################################################################
-
-    if args.rhost:
-        subject = args.rhost
-    elif args.user:
-        subject = args.user
-
-    if args.type:
-        record_list = format_record(unformatted_login_rec)
-        if args.type == 'daily':
-            cal_daily_usage(subject,record_list)
-        elif args.type == 'weekly':
-            cal_weekly_usage(subject,record_list)
-        else:
-            cal_monthly_usage(subject,record_list)
+        if args.type:
+            record_list = format_record(unformatted_login_rec)
+            if args.type == 'daily':
+                cal_daily_usage(subject,record_list,option)
+            elif args.type == 'weekly':
+                cal_weekly_usage(subject,record_list,option)
+            else:
+                cal_monthly_usage(subject,record_list,option)
