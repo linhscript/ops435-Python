e119d9186962f5279a8ba8aed4651a2478f63530
Linh Ha Van <havanlinh304@gmail.com>
2019-03-07 23:30:53 -0500

    Update test3.py

--- Assignment/test3.py
+++ Assignment/test3.py
@@ -1,41 +1,13 @@
 '''
-Files to be processed: ['a2_test_data_2']
-Type of args for files <class 'list'>
-
-usage report for user: user5
-usage report type: weekly
-processing usage report for the following:
-reading login/logout record files ['a2_test_data_2']
-
-Files to be processed: ['a2_test_data_2']
-Type of args for files <class 'list'>
-
-processing usage report for the following:
-reading login/logout record files ['a2_test_data_2']
-Generating list for host
 
 '''
-def gen_text(option):
-	if option == True:
-		print("Files to be processed: ",args.filename)
-		print("Type of args for files ",type(args.filename))
-
-		if args.list:
-			print("usage report for user: ",subject)
-			print("usage report type: ",args.type)
-			print("processing usage report for the following:")
-			print("reading login/logout record files ",args.filename)
-		else:
-			print("processing usage report for the following:")
-			print("reading login/logout record files ",args.filename)
-			print("Generating list for ",args.list)
 
 
 if __name__ == '__main__':
 	import time
 	import os
-	#f = open('test_data','r')
-	#data = f.read()
-	#f.close()
-	#print(data.split())
+	f = open('OPS435 Python A3 CCN Subscription - CDOT Wiki','r')
+	data = f.read()
+	f.close()
+	print(data.split())
 	gen_text()
