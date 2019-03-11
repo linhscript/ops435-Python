7fac141bfe1cc897fa80b347812a54cafb4e4baf
Linh Ha Van <havanlinh304@gmail.com>
2019-03-11 15:06:44 -0400

    8

--- Assignment/test2.py
+++ Assignment/test2.py
@@ -1,11 +1,8 @@
-data = {'2018 02 13':1580,'2018 02 14':84657,'2018 02 15':1563}
-subject = 'rchan'
-text = "Daily Usage Report for "
-l = len(text+str(subject))
-print(text+str(subject))
-print(l*'=')
-print("Date"+" "*(l//2)+"Usage in Seconds")
-for key in sorted(data.keys(),reverse=True):
-    print(str(key) +" "*(l//2)+ str(data[key]))
-print("Total" +" "*(l//2),15324)
+list1 = ['1','2','3']
+list2 = ['a','b','c']
 
+list3 = [1,2,3,4,5,6,7,8]
+
+new_list = list(map((lambda x:x<5),list3))
+
+print (new_list)
