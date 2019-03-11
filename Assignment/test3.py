0698d877aaf0cc85458a19fc74bc0f38e27079c8
Linh Van Ha <havanlinh304@gmail.com>
2019-03-11 16:55:23 -0400

    656

--- Assignment/test3.py
+++ Assignment/test3.py
@@ -6,8 +6,12 @@
 if __name__ == '__main__':
 	import time
 	import os
-	f = open('OPS435 Python A3 CCN Subscription - CDOT Wiki','r')
-	data = f.read()
+	f = open('filename.txt','r')
+	data = f.readlines()
 	f.close()
-	print(data.split())
-	gen_text()
+	data = ' '.join(map(str.strip,data))
+	print(data)
+	print("")
+	data = data.split('</tr>')
+	data = data[0].split()
+	print(data)
