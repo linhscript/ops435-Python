4f2ac85af41c58f8a29fc5762e08f1d19386abad
Van Linh Ha <vlha@myseneca.ca>
2019-03-14 09:16:15 -0400

    45

--- Assignment/test.py
+++ Assignment/test.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python3
-'''
-OPS435 Assignment 2 - Winter 2018
-Program: ur_vlha.py
-Author: "Van Linh Ha"
-The python code in this file ur_vlha.py is original work written by
-"Van Linh Ha". No code in this file is copied from any other source 
-including any person, textbook, or on-line resource except those provided
-by the course instructor. I have not shared this python file with anyone
-or anything except for submission for grading.  
-I understand that the Academic Honesty Policy will be enforced and violators 
-will be reported and appropriate action will be taken.
-'''
-
 import os 
 import sys
 import time
--- Assignment/test2.py
+++ Assignment/test2.py
@@ -5,4 +5,5 @@ list3 = [1,2,3,4,5,6,7,8]
 
 new_list = list(map((lambda x:x<5),list3))
 
-print (new_list)
+def gen():
+	return "List1",list1,"List2",list2
