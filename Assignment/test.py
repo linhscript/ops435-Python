907801af1ff8accbc76be618f53f116aec4e12bb
Van Linh Ha <vlha@myseneca.ca>
2019-03-14 09:55:54 -0400

    87

--- Assignment/test.py
+++ Assignment/test.py
@@ -35,51 +35,6 @@ def get_list (list_record,position):
     #   print(i)
     print(*sorted(res_list),sep = "\n")
 
-def format_record(unformat_record):
-
-    record_list = []
-    for item in unformat_record:
-        time1 = time.strptime(' '.join(item.split()[3:8]),"%a %b %d %H:%M:%S %Y")
-        time2 = time.strptime(' '.join(item.split()[9:14]),"%a %b %d %H:%M:%S %Y")
-        # time1 and time2 is struct_time
-        doy1 = time.strftime("%j",time1) # return day of year
-        doy2 = time.strftime("%j",time2)
- 
-        if doy1 == doy2: # if same day
-            record_list.append(item.split()) # save the record to list
-        else: # this works even jump to many days
-            next_day = time.mktime(time1) # float number
-            
-            eod_time = time.ctime(next_day).split() #convert to readable time
-            old_day = item.split()
-
-            old_day[9] = eod_time[0]
-            old_day[10] = eod_time[1]
-            old_day[11] = eod_time[2]
-            old_day[12] = '23:59:59'
-            old_day[13] = eod_time[4]                           
-            record_list.append(old_day)
-            while doy1 != doy2:
-                                
-                next_day = next_day + 86400 #floating number
-                new_day = item.split()
-                new_time = time.ctime(next_day).split() # covert to readable time
-                new_day[3] = new_time[0]
-                new_day[4] = new_time[1]
-                new_day[5] = new_time[2]
-                new_day[6] = '00:00:00'
-                new_day[7] = new_time[4]
-                doy1 = time.strftime('%j',time.localtime(next_day))
-
-                if doy1 != doy2: # check if next day is on the same day with end day or not
-                    new_day[9] = new_time[0]
-                    new_day[10] = new_time[1]
-                    new_day[11] = new_time[2]
-                    new_day[12] = '23:59:59'
-                    new_day[13] = new_time[4]
-                record_list.append(new_day)
-    return record_list
-
 def cal_daily_usage(subject,login_recs):
     ''' docstring for this function
     generate daily usage report for the given 
--- Assignment/test2.py
+++ Assignment/test2.py
@@ -1,9 +1,22 @@
 list1 = ['1','2','3']
 list2 = ['a','b','c']
+list3 = [1,2,3,4,5,6,7,8,'e','g','h']
 
-list3 = [1,2,3,4,5,6,7,8]
-
-new_list = list(map((lambda x:x<5),list3))
+tests = { 1:['20180101 1','20180102'],
+         2:['20180101 -1','20171231'],
+         3:['20180101 2','20180103'],
+         4:['--step 20180101 3','20180102\n20180103\n20180104\n'],
+         5:['20180701 500','20191113'],
+         6:['20189901 2','Error: wrong month entered'],
+         7:['20180199 2','Error: wrong day entered'],
+         8:['2018 2','Error: wrong date entered']
+        }
+#new_list = list(map((lambda x:x<5),list3))
 
 def gen():
-	return "List1",list1,"List2",list2
+	#return (sorted(list2))
+	print(*sorted(list2),sep = "\n")
+
+if __name__ == '__main__':
+
+	print(tests[8])
