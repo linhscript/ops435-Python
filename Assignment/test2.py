e55d7717da6df3c48b71ebbf0ae9b961ad86043d
Linh Ha Van <havanlinh304@gmail.com>
2019-03-15 00:01:13 -0400

    55

--- Assignment/test2.py
+++ Assignment/test2.py
@@ -1,11 +1,22 @@
 import os
+from twilio.rest import Client
 
-list1 = ['1','2','3']
-list2 = ['a','e','c','b','h']
-list3 = [1,2,3,4,5,6,7,8,'e','g','h']
-text = []
 
+account_sid = "AC60c06b56f769a6d030463edd5f9e2809" #Your Twilio account ID
+auth_token = "d6bfdd719f8f7aaa0ec98c8a2da1f815"    #Your secret API Token
+ 
+client = Client(account_sid, auth_token)
 
 
 if __name__ == '__main__':
-    print(os.getcwd())
+    print("SMS sent")
+
+    message = client.messages \
+                    .create(
+                         body="Test2",
+                         from_='whatsapp:+14155238886',
+                         to='whatsapp:+16479067569'
+                         
+                     )
+
+    print(message.sid)
