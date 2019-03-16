0f7b6e35f30f6e51a8fc35cd944ee1979533312a
Linh Ha Van <havanlinh304@gmail.com>
2019-03-15 23:18:29 -0400

    5

--- Assignment/test.py
+++ Assignment/test.py
@@ -5,7 +5,7 @@ import os
 
 if __name__ == '__main__':
 
-    f = open('a2_test_run_2_results.txt','r')
+    f = open('test_file','r')
     data = f.readlines()
     f.close()
     commands = []
@@ -13,15 +13,14 @@ if __name__ == '__main__':
     num = 0
     list_result = []
     for item in data:
-        if item.startswith('+'):
-            commands.append(item[2::].strip())
-            num = num + 1
-            list_result.clear()
-        else:
-            list_result.append(item.strip())
 
+        if item.startswith('+'):
+            #commands.append(item[2::].strip())
+            list_result.clear()
+            num = num + 1
+        else:  
+            list_result.append(item.strip())
             result[num] = list_result
-            print(result)
         if num == 3:
-            break 
+            break
     print(result)
