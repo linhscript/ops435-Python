7e7a9c54ea3af81295dd3384eeb6741935560222
Linh Ha Van <havanlinh304@gmail.com>
2019-03-17 23:33:36 -0400

    4234

--- Assignment/test.py
+++ Assignment/test.py
@@ -6,4 +6,28 @@ import subprocess
 
 if __name__ == '__main__':
 
- 
+    f = open('a2_test_run_2_results.txt','r')
+    data = f.readlines()
+    f.close()
+    result = {}
+    list_result = []
+    for item in data:
+        if item.startswith('+'):
+            commands =item.strip()
+            list_result = list_result.copy()            
+            list_result.clear()  
+        else:  
+            list_result.append(item.strip())
+            result[commands] = list_result
+
+
+    #print(result)
+    c = result["+ ./ur.py -l user a2_test_data_2"]
+    cmd = 'python a2_vlha.py -l user a2_test_data_2'
+    #print('Test run command',test_no,':',cmd)
+    p1 = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+    test = p1.communicate()[0].decode('utf-8').strip('\n')    
+    print(c)
+    print("")
+    print(test.split("\n"))
+    print(c == test.split("\n"))
