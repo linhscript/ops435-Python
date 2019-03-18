3123b3b753bd8216288e06579703c752d434d82f
Van Linh Ha <vlha@myseneca.ca>
2019-03-18 08:30:22 -0400

    423

--- Assignment/check_script.py
+++ Assignment/check_script.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
     total_test_marks = 0
     for item in test_marks:
         total_test_marks += test_marks[item] 
-    total_test_marks = total_test_marks / 17 * 46
+    total_test_marks = total_test_marks / 17 * 100 ## 17 commands to check
     print('Total test run marks: ',total_test_marks)
     grand_total = total_test_marks + total_doc_marks
-    print('Total marks for script (max. 46):',grand_total) 
+    print('Total marks for script (max. 100):',grand_total) 
