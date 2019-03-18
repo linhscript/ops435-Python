e09b10ed61a4eeafab4328ecf3648dda123e3ad6
Linh Ha Van <havanlinh304@gmail.com>
2019-03-18 18:05:19 -0400

    32

--- Assignment/test.py
+++ Assignment/test.py
@@ -18,16 +18,16 @@ if __name__ == '__main__':
             list_result = list_result.copy()            
             list_result.clear()  
         else:  
-            list_result.append(item.strip())
+            list_result.append(item)
             result[commands] = list_result
 
 
     print(result['+ ./ur.py -h'])
     # c = result["+ ./ur.py -u user5 -t daily a2_test_data_2 -v"]
-    # cmd = 'python3.6 a2_vlha.py -u user5 -t daily a2_test_data_2 -v'
-    # #print('Test run command',test_no,':',cmd)
-    # p1 = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
-    # test = p1.communicate()[0].decode('utf-8').strip('\n')    
+    cmd = 'python3.6 a2_'+student+'.py'+ '-h'
+    print('Test run command',':',cmd)
+    p1 = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+    result_fromcmd = p1.communicate()[0].decode('utf-8').strip('\n') 
     # print(c)
     # print("")
     # print(test.split("\n"))
