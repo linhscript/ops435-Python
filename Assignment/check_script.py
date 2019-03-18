52e74b8861072641eb16bbf05c7b5f411cdf95e8
Linh Ha Van <havanlinh304@gmail.com>
2019-03-18 19:26:30 -0400

    54234

--- Assignment/check_script.py
+++ Assignment/check_script.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     total_test_marks = 0
     for item in test_marks:
         total_test_marks += test_marks[item] 
-    total_test_marks = total_test_marks / 17 * 100 ## 17 commands to check
+    total_test_marks = total_test_marks / 16 * 100 ## 17 commands to check
     print('Total test run marks: ',total_test_marks)
     grand_total = round((total_test_marks + total_doc_marks),2)
     print('Total marks for script (max. 100):',color.UNDERLINE,color.YELLOW,grand_total,color.END) 
