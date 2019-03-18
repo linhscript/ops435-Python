f8c803c6fd12e8b3205d2b40bcb82a0cb120c419
Linh Ha Van <havanlinh304@gmail.com>
2019-03-17 22:50:16 -0400

    656

--- Assignment/test.py
+++ Assignment/test.py
@@ -9,25 +9,40 @@ if __name__ == '__main__':
     f = open('a2_test_run_2_results.txt','r')
     data = f.readlines()
     f.close()
-    result = {}
-    list_result = []
+    tests = {}
+    list_coms = []
     for item in data:
         if item.startswith('+'):
-            commands =item.strip()
-            list_result = list_result.copy()            
-            list_result.clear()  
+            commands =item.replace('+ ','').strip()
+            list_coms = list_coms.copy()            
+            list_coms.clear()  
         else:  
-            list_result.append(item.strip())
-            result[commands] = list_result
+            list_coms.append(item.strip())
+            tests[commands] = list_coms
 
+    for com_test in tests.key():
+        if com_test.startswith('./ur'):
+            com_test.replace('./ur', student)
 
-    #print(result)
-    c = result["+ ./ur.py -l user a2_test_data_2"]
-    cmd = 'python3.6 test_main.py -l user a2_test_data_2'
-    #print('Test run command',test_no,':',cmd)
-    p1 = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
-    test = p1.communicate()[0].decode('utf-8').strip('\n')    
-    print(c)
-    print("")
-    print(test.split("\n"))
-    print(c == test.split("\n"))
+            cmd = 'python3 a2_'+student+'.py '+ com_test
+            print('Test run command',test_no,':',cmd)
+            p1 = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+            result = p1.communicate()[0].decode('utf-8').strip('\n')  
+            expected = test.split("\n")
+            if result == expected:
+                print('--test passed--')
+                test_marks[test_no] = 1
+            else:
+                print('--test failed--')
+                print('---- expect:',expected)
+                print('----  given:',result)
+                test_marks[test_no] = 0
+
+    print('Test Results:',test_marks)
+    total_test_marks = 0
+    for item in test_marks:
+    total_test_marks += test_marks[item] 
+    total_test_marks = total_test_marks / 19 * 46
+    print('Total test run marks: ',total_test_marks)
+    grand_total = total_test_marks + total_doc_marks
+    print('Total marks for script (max. 46):',grand_total)  
