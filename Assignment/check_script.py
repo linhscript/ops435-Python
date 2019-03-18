b4a7778f7d65737a2cd5576bfe76f220a4a49191
Linh Ha Van <havanlinh304@gmail.com>
2019-03-18 11:09:47 -0400

    43

--- Assignment/check_script.py
+++ Assignment/check_script.py
@@ -89,9 +89,9 @@ if __name__ == '__main__':
                 print(color.GREEN,'--test passed--',color.END)
                 test_marks[num] = 1
             else:
-                for x in range(0,len(result)-1):
-                    if result[x] != expected[x]:
-                        print(result[x])
+                # for x in range(0,len(result)-1):
+                #     if result[x] != expected[x]:
+                #         print(result[x])
                 print(color.RED,color.BOLD,'--test failed--',color.END)
                 print(color.BLUE,color.UNDERLINE,'---- expect:',color.END,expected)
                 print(color.BLUE,color.UNDERLINE,'----  given:',color.END,result)
