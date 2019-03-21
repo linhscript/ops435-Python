48efa715cca6673ffcb4f9f33f48d6b419cbe94e
Linh Ha Van <havanlinh304@gmail.com>
2019-03-20 22:49:58 -0400

    32

--- Assignment/test2.py
+++ Assignment/test2.py
@@ -3,7 +3,12 @@ import sys
 l1 = ['a','b','c']
 l2 = [1,2,3]
 l2[0] = 6
-l5.split('\n')[1] = 'AAA'
+#l5.split('\n')[1] = 'AAA'
 result = {}
 num = []
-d = {} 
+d = {'a':1,'b':2,'c':3,'d':4}
+
+
+
+a= list(d.values())
+print(a)
